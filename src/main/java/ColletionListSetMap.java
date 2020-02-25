60174a81494aa5917d93211d49298206b6eabf2a
ClaytonClaassensDev <217299695@mycput.ac.za>
2020-02-26 00:34:47 +0200

    initial commit

---
+++ src/main/java/ColletionListSetMap.java
@@ -0,0 +1,57 @@
+/**
+ * Ryan Petersen 217027806
+ * 3G
+ * Chapter 2 Assignment Question 1
+ *
+ */
+import java.util.*;
+public class ColletionListSetMap {
+
+    private List<Integer> numbers;
+    private HashSet names;
+    private HashMap cityCountry;
+
+    public ColletionListSetMap() {
+
+        //List
+        numbers = new ArrayList();
+        numbers.add(12);
+        numbers.add(7);
+        numbers.add(22);
+        Collections.sort(numbers);
+
+        //Set
+        names = new HashSet();
+        names.add("David");
+        names.add("Luke");
+        names.add("Riyaad");
+        //sort = new TreeSet(names);
+
+
+        //Map
+        cityCountry = new HashMap();
+        cityCountry.put("Cape Town", "South Africa");
+        cityCountry.put("Luanda", "Angola");
+        cityCountry.put("Yaounde", "Cameroon");
+    }
+
+    public List<Integer> getList() {
+        return numbers;
+    }
+
+    public HashSet<String> getSet() {
+        return names;
+    }
+
+    public HashMap<String, String> getMap() {
+        return cityCountry;
+    }
+
+        public static void main (String[]args)
+        {
+            ColletionListSetMap clsm = new ColletionListSetMap();
+            System.out.println("List sorted from smallest to biggest: " + clsm.getList());
+            System.out.println("Set sorted alphabetically: " + clsm.getSet());
+            System.out.println("Map" + clsm.getMap());
+        }
+}
---
+++ src/test/java/CollectionListSetMapTest.java
@@ -0,0 +1,93 @@
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple CollectionListSetMap.
+ * Clayton Claassens 217299695
+ * 3A
+ * Assignment2
+ */
+public class CollectionListSetMapTest
+{
+    public ColletionListSetMap collectionListSetMap;
+    private String failMessage = "This is my failing test message";
+
+    @Test
+    public void testList()
+    {
+
+        collectionListSetMap = new ColletionListSetMap();
+        List<Integer> actualList = collectionListSetMap.getList();
+        List<Integer> expectedList = Arrays.asList(7,12,22);
+
+        //ListTest
+        assertEquals(actualList,expectedList);
+        assertThat(actualList,hasItems(7));
+        assertThat(actualList,hasItems(12));
+        assertThat(actualList,hasItems(22));
+
+
+    }
+
+    @Test
+    public void testSet(){
+
+        collectionListSetMap = new ColletionListSetMap();
+
+        HashSet<String> actualSet = collectionListSetMap.getSet();
+        HashSet<String> expectedSet = new HashSet<String>();
+        expectedSet.add("David");
+        expectedSet.add("Luke");
+        expectedSet.add("Riyaad");
+
+        //SetTest
+        assertEquals(actualSet, expectedSet);
+        assertThat(actualSet,is(expectedSet));
+        assertThat(actualSet,hasItems("Riyaad"));
+
+
+    }
+    //@Ignore
+    @Test (timeout = 1000)
+    public void testMap(){
+
+        collectionListSetMap = new ColletionListSetMap();
+
+        HashMap<String, String> actualMap = collectionListSetMap.getMap();
+        HashMap<String, String> expectedMap = new HashMap();
+        expectedMap.put("Cape Town", "South Africa");
+        expectedMap.put("Luanda", "Angola");
+        expectedMap.put("Yaounde", "Cameroon");
+
+        //MapTest
+        assertThat(actualMap,is(expectedMap));
+        assertThat(actualMap.size(),is (3));
+
+
+    }
+
+    @Test
+    public void failTest(){
+
+        fail(failMessage);
+
+    }
+
+    @Ignore
+    @Test
+    public void disablingTest(){
+
+        System.out.print("DisablingTest");
+    }
+}
